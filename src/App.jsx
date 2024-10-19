cfcc35b83b8f51ab43cc77c9c932dbf047f72650
mehedi hasan <mhshohan04@gmail.com>
2024-10-19 16:15:39 +0600

    state reduce

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@
 import Form from './component/Form'
 import './style.css'
 
-  const statuses = [ 'empty', 'typing', 'submitting', 'success', 'error'];
 
 function App() {
 
   return (
     <>
 
-        {
-          statuses.map((status) => (
-            <Form key={status} status={status}></Form>
-          ))
-        }
+            <Form></Form>
 
     </>
   )
